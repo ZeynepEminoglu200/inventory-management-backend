8273f6e0aa6db0245eb37678ef7f3fd6a6628e6a
Zeynep Eminoglu <zeynep.eminoglu@clearscore.com>
2026-03-29 20:23:05 +0100

    adding in extra validiation

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -3,11 +3,22 @@ from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8)
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("This username is already taken.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
