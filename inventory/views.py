988a974d5174db69b4fb9440b654305da3ba7402
Zeynep Eminoglu <zeynep.eminoglu@clearscore.com>
2026-04-25 07:24:10 +0100

    Add temporary superuser endpoint

--- inventory/views.py
+++ inventory/views.py
@@ -5,7 +5,8 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 @api_view(['POST'])
