a6c8c387457ef07d3816fab361d34a29ad787417
Zeynep Eminoglu <zeynep.eminoglu@clearscore.com>
2026-04-25 06:56:21 +0100

    Prepare backend for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
-    "https://your-frontend-name.onrender.com",
+    "https://inventory-management-frontend-8ope.onrender.com",
 ]
 
 ROOT_URLCONF = 'config.urls'
